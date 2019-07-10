1c200700c8dcd9a123aaa284b8cbf124be5a3365
github username <github email address>
2019-07-10 16:35:45 -0500

    updated cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,6 +13,7 @@ def welcome
     if user_input == "New"
         puts "Please create username"
         a = gets.chomp
+        puts `clear`
         new_user = User.create(:username => a)
         puts "Welcome #{a}"
         puts options(new_user)
@@ -41,16 +42,14 @@ def options(new_user)
         menu.choice 'Search recipe by calories'
         menu.choice 'View favorites'
         end
+        puts `clear`
         if  user == "Search recipe by keyword"
-            puts "What ingredient would you like to search recipes for? (Please enter number)"
+            puts "What ingredient would you like to search recipes for?"
             input = gets.chomp.capitalize
             search = Recipe.where("title like ?", "%#{input}%") #returns all Chicken recipes
             array = []
             search.each.with_index(1) do |s,i|
                 array << "#{i} #{s.title}"
-                ######put TTY prompt###############
-                # prompt = TTY::Prompt.new
-                # prompt.select("Select an editor?", s.title)
             end
             puts array
             selected_num = gets.chomp.to_i
@@ -74,15 +73,38 @@ def options(new_user)
             puts "How many calories would you like your meal to be within?"
             calories = gets.chomp.to_f
             search = Recipe.where("calories < ?", calories)
-            search.collect do |s|
-                puts s.title
+            array = []
+            search.each.with_index(1) do |s,i|
+                array << "#{i} #{s.title}"
             end
+            puts array
+            selected_num = gets.chomp.to_i
+                if selected_num == 1
+                    x = Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 2
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 3
+                    x = Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 4
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 5
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                end
         elsif user == "View favorites"
-            # binding.pry
-            # search = Favorite.find_by (user_id: new_user.id)
-            # puts search
-            # puts NEW_LINE
-            #User can then enter each of these recipes  = If recipe has been selected, can un-favorite, or go back to homepage
+            puts "My Favorites"
+            puts view_favorites(new_user)
+            puts "Return home? (y/n)"
+                input = gets.chomp
+                if input == "y"
+                    puts `clear`
+                    puts options(new_user)
+                else ""
+                end
         else
             puts ''
         end
@@ -99,12 +121,13 @@ def chicken_select(x, new_user)
     puts "Calories - \n #{x.calories}"
     puts "\n"
     ################################ADD TTY PROMPT##########################################
-    puts "1. Add to Favorites"
+    puts "1. ♥ Add to Favorites"
     puts "2. Return to Home"
     input = gets.chomp.to_i
     if input == 1
-        puts create_favorite(new_user, x)
+        create_favorite(new_user, x)
         puts "Added #{x.title} to Favorites!"
+        puts `clear`
         puts options(new_user)
     elsif input == 2
         puts options
@@ -119,7 +142,21 @@ def create_favorite(new_user, recipe)
     new_fave = Favorite.new
     new_fave.user_id = new_user.id
     new_fave.recipe_id = recipe.id
-    new_fave
+    new_fave.save
 end
 
+def view_favorites(new_user)
+    get_my_faves = Favorite.where(user_id: new_user.id)
+    new_array = []
+    get_my_faves.each do |fave|
+        new_array << fave.recipe.id
+    end 
+    new_array.map do |id|
+        puts find_recipe_title(id)
+    end
+end
 
+def find_recipe_title(id)
+    a = Recipe.find(id)
+    a.title
+end
