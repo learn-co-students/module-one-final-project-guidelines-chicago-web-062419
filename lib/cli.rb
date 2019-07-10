1119d9f93cc274faa6b2ddad5d10200a017ee7a4
github username <github email address>
2019-07-10 15:18:06 -0500

    updated cli and recipes CHICKEN SELECT

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,64 +3,123 @@ require "tty-prompt"
 
 # puts `clear`
 
+NEW_LINE = "\n"
+
 def welcome
     
     puts "Welcome, hungry human!"
     prompt = TTY::Prompt.new
     user_input = prompt.select("What type of user are you?", %w(New Existing))
-
-    # user_input = gets.chomp
     if user_input == "New"
         puts "Please create username"
         a = gets.chomp
-        User.create(username: a)
-        puts "Welcome #{a}, what would you like to do today?"
-        puts options
-            selection = gets.chomp
-            if selection.to_i == 1
-                puts search_recipes_by_ingredient
-            elsif selection.to_i == 2
-                puts search_recipes_by_calories
-            elsif selection.to_i == 3
-                puts view_favorites
-            else
-                puts ''
-            end
+        new_user = User.create(:username => a)
+        puts "Welcome #{a}"
+        puts options(new_user)
+        # binding.pry
     elsif user_input == "Existing"
         puts "Please enter username"
         name = gets.chomp
-        puts "Welcome back, #{name}. What would you like to do today?"
-        puts options
-    else puts ''
+        puts "Welcome back, #{name}."
+        puts options(new_user)
+        else puts ''
     end
-    # binding.pry
-    # clear the screen for next method.
 end
+    
+# def search_recipes_by_calories
+#     puts "Calorie counting? How many calories would you like your recipe to be within?"
+#     input = gets.chomp.to_f
+#     Recipe.search_by_calories(input)
+# end
 
-def search_recipes_by_ingredient
-    puts "What ingredient would you like to search recipes for?"
-    input = gets.chomp.capitalize
-    search = Recipe.where("title like ?", "%#{input}%")
-    search.title
-    # binding.pry
-end
-
-def search_recipes_by_calories
-    puts "Calorie counting? How many calories would you like your recipe to be within?"
-    input = gets.chomp.to_f
-    Recipe.search_by_calories(input)
-end
-
-def view_favorites
-
-end
-
-def options
+def options(new_user)
+    puts "HOME"
     prompt = TTY::Prompt.new
-    user_input = prompt.select("What type of user are you?", %w(New Existing))
-    puts "1. Search recipes by keyword"
-    puts "2. Search recipes by calories"
-    puts "3. View favorited recipes"
+    # user_input = prompt.select("What would you like to do today?", %w(Search recipes by keyword "Search recipes by calories", "View favorited search_recipes_by_ingredient"))
+    user = prompt.select("What would you like to do today?") do |menu|
+        menu.choice 'Search recipe by keyword'
+        menu.choice 'Search recipe by calories'
+        menu.choice 'View favorites'
+        end
+        if  user == "Search recipe by keyword"
+            puts "What ingredient would you like to search recipes for? (Please enter number)"
+            input = gets.chomp.capitalize
+            search = Recipe.where("title like ?", "%#{input}%") #returns all Chicken recipes
+            array = []
+            search.each.with_index(1) do |s,i|
+                array << "#{i} #{s.title}"
+                ######put TTY prompt###############
+                # prompt = TTY::Prompt.new
+                # prompt.select("Select an editor?", s.title)
+            end
+            puts array
+            selected_num = gets.chomp.to_i
+                if selected_num == 1
+                    x = Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 2
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 3
+                    x = Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 4
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                elsif selected_num == 5
+                    x =  Recipe.find(selected_num)
+                    puts chicken_select(x, new_user)
+                end
+        elsif user == "Search recipe by calories"
+            puts "How many calories would you like your meal to be within?"
+            calories = gets.chomp.to_f
+            search = Recipe.where("calories < ?", calories)
+            search.collect do |s|
+                puts s.title
+            end
+        elsif user == "View favorites"
+            # binding.pry
+            # search = Favorite.find_by (user_id: new_user.id)
+            # puts search
+            # puts NEW_LINE
+            #User can then enter each of these recipes  = If recipe has been selected, can un-favorite, or go back to homepage
+        else
+            puts ''
+        end
+        # binding.pry
+end
+
+
+
+def chicken_select(x, new_user)
+    puts `clear`
+    puts "Title - \n #{x.title}"
+    puts "Ingredients - \n #{x.ingredients}"
+    puts "Directions - \n #{x.directions}"
+    puts "Calories - \n #{x.calories}"
+    puts "\n"
+    ################################ADD TTY PROMPT##########################################
+    puts "1. Add to Favorites"
+    puts "2. Return to Home"
+    input = gets.chomp.to_i
+    if input == 1
+        puts create_favorite(new_user, x)
+        puts "Added #{x.title} to Favorites!"
+        puts options(new_user)
+    elsif input == 2
+        puts options
+    else puts "Bok bok, try again :("
+        ########################ADD ASSCI of DEAD CHICKEN#####################################
+    end
+end
+
+
+
+def create_favorite(new_user, recipe)
+    new_fave = Favorite.new
+    new_fave.user_id = new_user.id
+    new_fave.recipe_id = recipe.id
+    new_fave
 end
 
 
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,9 +3,22 @@ class Recipe < ActiveRecord::Base
     has_many :users, through: :favorites
 
     #User can search all recipes using keyword
-    def self.search_by_ingredient(argument)
-        search = Recipe.where("title like ?", "%#{argument}%")
-        search
+    # def self.search_by_ingredient(argument)
+    #     search = Recipe.where("title like ?", "%#{argument}%")
+    #     search
+    # end
+
+    def search_recipes_by_ingredient
+        puts "What ingredient would you like to search recipes for?"
+        input = gets.chomp.capitalize
+        search = Recipe.where("title like ?", "%#{input}%") #returns all Chicken recipes
+        puts search.title
+        # binding.pry
+    end
+
+    def display_recipe(argument)
+        search = Recipe.find_by title: argument 
+        puts search
     end
 
 #User can search by max calories
@@ -13,10 +26,11 @@ class Recipe < ActiveRecord::Base
         Recipe.where("calories < ?", calories)
     end
 #Recipe can see how many times their recipe has been favorited
-    def favorites_count
-       faves =  Favorite.where(recipe_id: self.id)
+    def favorites_count(recipe)
+       id = recipe.id
+       faves =  Favorite.where(recipe_id: id)
        faves.count
     end
 
-end
 
+end
